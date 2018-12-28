07b9c16c71bee3dad821c2c0fe4b5600aa9f0a37
EC2 Default User <ec2-user@ip-172-31-37-241.us-east-2.compute.internal>
2018-12-28 23:30:36 +0000

    setup start script and heroku port

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -52,7 +53,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
 app.disable('etag');
